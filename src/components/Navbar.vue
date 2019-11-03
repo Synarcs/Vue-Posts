8eb0f225a7a84aaffb365a933d50aa1ed9f835db
vedangparasnis <vedang.parasnis921@gmail.com>
2019-11-03 21:27:27 +0530

    ./

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar navbar-expand navbar-light bg-dark text-secondary">
+    <div class="container">
+      <div class="navbar-brand text-warning">Home Page</div>
+      <div class="content ml-auto">
+        <div class="btn btn-transparent">
+          <a href="/" class="nav-link">Home</a>
+        </div>
+        <div class="btn btn-transparent">
+          <a href="/about" class="nav-link">About</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/route.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+// components
+import About from "./views/About";
+import Home from "./views/Home";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      component: Home
+    },
+    {
+      path: "/about",
+      component: About
+    }
+  ]
+});
